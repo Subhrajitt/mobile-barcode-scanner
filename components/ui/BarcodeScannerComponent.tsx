99c4963b77fac5cb1d08e3895b60126894abcf9f
unknown <Subhrajit Roy>
2025-06-27 11:12:35 +0530

    modification in app router file

--- components/ui/BarcodeScannerComponent.tsx
+++ components/ui/BarcodeScannerComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { BarcodeScanner } from 'react-barcode-detection';
 import { Button } from './button';
 
